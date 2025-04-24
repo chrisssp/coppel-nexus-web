0b3c96f0474f6e5434d833e12578e35f429ee6bd
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-04-24 06:34:03 -0600

    Add Navbar component and update layout and home page structure

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full h-14 bg-blue-300 bg-z shadow-md flex items-center justify-between px-6">
+      <span className="">Coppel Nexus</span>
+      {/* contenido */}
+    </nav>
+  );
+}
